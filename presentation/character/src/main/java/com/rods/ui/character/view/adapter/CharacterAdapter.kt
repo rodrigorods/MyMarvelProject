47bc8e1ca1b0e198985989c3247a388d5f9e9219
rodrigorods <rodrigo.otavio89@gmail.com>
2020-11-09 00:05:22 -0300

    add image load with glide

--- presentation/character/src/main/java/com/rods/ui/character/view/adapter/CharacterAdapter.kt
+++ presentation/character/src/main/java/com/rods/ui/character/view/adapter/CharacterAdapter.kt
@@ -3,8 +3,10 @@ package com.rods.ui.character.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.rods.domain.character.model.MarvelCharacter
 import com.rods.ui.character.R
 
@@ -29,10 +31,19 @@ class CharacterAdapter(
 
         private var name: TextView = rootView.findViewById(R.id.name)
         private var description: TextView = rootView.findViewById(R.id.description)
+        private var thumbnail: ImageView = rootView.findViewById(R.id.thumbnail)
 
         fun bind(character: MarvelCharacter) {
             name.text = character.name
             description.text = character.description
+            thumbnail.download(character.thumbnailUrl)
+        }
+
+        private fun ImageView.download(imageLink: String) {
+            Glide.with(context)
+                .load(imageLink)
+                .fitCenter()
+                .into(this)
         }
     }
 }
